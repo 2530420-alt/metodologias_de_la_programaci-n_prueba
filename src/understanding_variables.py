6dc454bb3a65e7afaf5985eca56c2e1ac440d994
Joan_orlando <2530420@upv.edu.mx>
2025-10-29 09:18:58 -0600

    feat: first class

---
+++ src/understanding_variables.py
@@ -0,0 +1,12 @@
+message = "this is my first variable"
+another_message = "this is another variable"
+
+print(message)
+print(another_message)
+
+print(message, another_message)
+
+"""
+Los nombres de varriables en Python deben nombrarse solo con:
+      - letras, números y guiones bajos (_)
+      deben 
